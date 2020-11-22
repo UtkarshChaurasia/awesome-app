cdc711bd2356f08f9a6b851e0a0f4eab5888d992
UtkarshChaurasia <msdianuc07@gmail.com>
2020-11-22 17:57:24 +0530

    ch2-containers and text

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,36 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(
-        child: Center(child: Text("Hello Flutter")),
-        color: Colors.yellow,
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 5,
+                      offset: Offset(2.0, 5.0))
+                ],
+                color: Colors.amber,
+                gradient: LinearGradient(
+                    colors: [Colors.red, Colors.orange, Colors.pink])),
+            child: Text(
+              "I am a box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ));
   }
 }
