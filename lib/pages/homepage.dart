f3767ebf9896b4b68f13d4802d5a266b46ec9ee4
UtkarshChaurasia <msdianuc07@gmail.com>
2020-11-29 00:47:07 +0530

    data fetched from API and listview builder

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../drawer.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,9 +11,19 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "LOG IN";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
   }
 
   @override
@@ -24,10 +35,25 @@ class _HomepageState extends State<Homepage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            //elevation: (10),
-            child: Column(
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+        //elevation: (10),
+        /*child: Column(
               children: <Widget>[
                 Image.asset(
                   "assests/bg.jpeg",
@@ -114,9 +140,7 @@ class _HomepageState extends State<Homepage> {
                   height: 50,
                 ),
               ],
-            ),
-          ),
-        ),
+            ),*/
       ),
       drawer: myDrawer(),
       floatingActionButton: FloatingActionButton(
